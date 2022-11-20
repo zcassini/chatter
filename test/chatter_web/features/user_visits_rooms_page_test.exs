5b765941f68959224105167ce83fab8dc6ae535d
zcassini <zcassini@gmail.com>
2022-11-20 13:07:55 -0500

    refactor

--- test/chatter_web/features/user_visits_rooms_page_test.exs
+++ test/chatter_web/features/user_visits_rooms_page_test.exs
@@ -2,16 +2,20 @@ defmodule ChatterWeb.UserVisitsRoomsPageTest do
   use ChatterWeb.FeatureCase, async: true
 
   test "user can visits rooms page to see a list of rooms", %{session: session} do
-    room = insert(:chat_room)
-
-  # test "GET /", %{conn: conn} do
-  #   conn = get(conn, ~p"/")
-  #   assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
-  # end
+    [room1, room2] = insert_pair(:chat_room)
     # |> visit(Routes.chat_room_path(@endpoint, :index))
 
     session
-    |> visit(~p"/")
-    |> assert_has(Query.css(".room", text: room.name))
+    |> visit(rooms_index())
+    |> assert_has(room_name(room1))
+    |> assert_has(room_name(room2))
+  end
+
+  defp rooms_index do
+    ~p"/"
+  end
+
+  defp room_name(room) do
+    Query.data("role", "room", text: room.name)
   end
 end
